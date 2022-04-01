2a42e87ccd492638601e081a288a6cf2aaf261b5
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-04-01 15:41:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function start()
 navigator.mediaDevices.getUserMedia({
 audio:true  
 });
-classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/M4fnAA_KG/model.json',ModelLoaded);
+classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/zk7TKkqpM/model.json',ModelLoaded);
 }
 function ModelLoaded()
 {
@@ -25,5 +25,14 @@ document.getElementById("result_label").innerHTML="I Can Hear - "+ results[0].la
 document.getElementById("result_accuracy").innerHTML="Accuracy - "+ (results[0].confidence * 100).toFixed(2)+" %";
 document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
 document.getElementById("result_accuracy").style.color="rgb("+r+","+g+","+b+")";
+img=document.getElementById("Ear");
+if(results[0].label=="bark")
+{
+img.src="https://media1.giphy.com/media/m9pQ6KapT7Cq3DQ5DZ/200.gif"    
+}
+else if (results[0].label=="meow")
+{
+img.src="https://media0.giphy.com/media/13lWraa7dfb7G0/giphy.gif"    
+}
 }
 }
